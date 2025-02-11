7bc0cab1d8471c0da8cb2f0e498f2e43d120fa31
[object Object] <tz4678@gmail.com>
2025-02-11 08:10:17 +0300

    .

--- logger/logger.go
+++ logger/logger.go
@@ -7,31 +7,31 @@ import (
 
 var l = log.New(os.Stderr, "", log.LstdFlags)
 
-func PrintColor(color, format string, v ...interface{}) {
+func printColor(color, format string, v ...interface{}) {
 	l.Printf(color+format+Reset, v...)
 }
 
 func Success(format string, v ...interface{}) {
-	PrintColor(BrightGreen, format, v...)
+	printColor(BrightGreen, format, v...)
 }
 
 func Warn(format string, v ...interface{}) {
-	PrintColor(BrightYellow, format, v...)
+	printColor(BrightYellow, format, v...)
 }
 
 func Error(format string, v ...interface{}) {
-	PrintColor(BrightRed, format, v...)
+	printColor(BrightRed, format, v...)
 }
 
 func Info(format string, v ...interface{}) {
-	PrintColor(BrightBlue, format, v...)
+	printColor(BrightBlue, format, v...)
 }
 
 func Log(format string, v ...interface{}) {
-	PrintColor(BrightWhite, format, v...)
+	printColor(BrightWhite, format, v...)
 }
 
 func Fatal(format string, v ...interface{}) {
-	PrintColor(Bold+Red, format, v...)
+	printColor(Bold+Red, format, v...)
 	os.Exit(1)
 }
